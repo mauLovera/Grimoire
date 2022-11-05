b283136bb14894b6226d277ad6514bf404402ad5
mauLovera <mauricioloveraedu@gmail.com>
2022-11-05 14:24:28 -0400

    feat: add home page working llayout and links

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 
+import styles from './Layout.module.scss'
+
 export default function Layout({ children, title, keywords, description }) {
   return (
     <>
       <Head></Head>
       <Header />
-      <main>{children}</main>
+      <main className={styles.container}>{children}</main>
       {/* <Footer /> */}
     </>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -12,15 +12,46 @@ export default function Home() {
         <h2>What are you looking for?</h2>
       </section>
       <section className={styles.content}>
-        <Link href="/classes">
-          <div className={styles.box}>Classes</div>
-        </Link>
-        <Link href="/monsters">
-          <div className={styles.box}>Monsters</div>
-        </Link>
-        <Link href="/spells">
-          <div className={styles.box}>Spells</div>
-        </Link>
+        <div className={styles.boxContainer}>
+          <h3>Classes</h3>
+          <p>
+            Class is the primary definition of what your character can do. It’s
+            more than a profession; it’s your character’s calling. Class shapes
+            the way you think about the world and interact with it and your
+            relationship with other people and powers in the multiverse.{' '}
+          </p>
+          <Link href="/classes">
+            <button>See More</button>
+          </Link>
+        </div>
+        <div className={styles.boxContainer}>
+          <h3>Monsters</h3>
+          <p>
+            Having various sizes, strengths, and weaknesses, a monster is the
+            most common adversary your characters will face in the world.
+            Knowing their skills, type, speed, saving throws, and hit points is
+            crucial to your character{"'"}s survival and management of them as
+            an ever-present threat.
+          </p>
+          <Link href="/monsters">
+            <button>See More</button>
+          </Link>
+        </div>
+        <div className={styles.boxContainer}>
+          <h3>Spells</h3>
+          <p>
+            A spell is a discrete magical Effect, a single shaping of the
+            magical energies that suffuse the multiverse into a specific,
+            limited expression. In casting a spell, a character carefully plucks
+            at the invisible strands of Raw Magic suffusing the world, pins them
+            in place in a particular pattern, sets them vibrating in a specific
+            way, and then releases them to unleash the desired effect—in most
+            cases, all in the span of seconds.
+          </p>
+          <Link href="/spells">
+            <button>See More</button>
+          </Link>
+        </div>
       </section>
     </Layout>
   )
