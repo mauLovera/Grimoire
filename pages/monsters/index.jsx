cb86b76dd7d9948ba6d962ae433221bbd93d19be
mauLovera <mauricioloveraedu@gmail.com>
2022-11-06 12:50:45 -0500

    feat: add monster and spell api call functions

--- pages/monsters/index.jsx
+++ pages/monsters/index.jsx
@@ -1,11 +1,33 @@
+import { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 import Layout from '@/components/Layout/Layout'
 
-import styles from '@/styles/pages/Monsters.module.scss'
+import styles from '@/styles/pages/classes/Classes.module.scss'
+import { getClassList } from 'services/api-calls'
 
-export default function MonstersPage() {
+export default function ClassesPage({ classes }) {
   return (
-    <Layout className={styles.container}>
-      Monsters
+    <Layout>
+      <section className={styles.banner}>
+        <h1>Monsters</h1>
+        <h2>What are they?</h2>
+      </section>
+      <section className={styles.container}>
+        <div className={styles.content}>
+          
+        </div>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await getClassList()
+  return {
+    props: {
+      classes: res,
+    },
+  }
 }
--- services/api-calls.js
+++ services/api-calls.js
@@ -1,5 +1,7 @@
 const baseUrl = 'https://www.dnd5eapi.co/api'
 
+// ====================================================== //
+
 export const getClassList = async () => {
   const res = await fetch(`${baseUrl}/classes`)
   return res.json()
@@ -9,4 +11,27 @@ export const getClassDetails = async (className) => {
   const res = await fetch(`${baseUrl}/classes/${className}`)
   return res.json()
 }
- 
+
+// ====================================================== //
+
+export const getMonsterList = async () => {
+  const res = await fetch(`${baseUrl}/monsters`)
+  return res.json()
+}
+
+export const getMonsterDetails = async (monsterName) => {
+  const res = await fetch(`${baseUrl}/monsters/${monsterName}`)
+  return res.json()
+}
+
+// ====================================================== //
+
+export const getSpellList = async () => {
+  const res = await fetch(`${baseUrl}/spells`)
+  return res.json()
+}
+
+export const getSpellDetails = async (spellName) => {
+  const res = await fetch(`${baseUrl}/spells/${spellName}`)
+  return res.json()
+}
