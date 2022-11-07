fbaeb324a4d6e391af21d3f29914f2e9ad2e7682
mauLovera <mauricioloveraedu@gmail.com>
2022-11-07 11:26:32 -0500

    feat & fix: add monsters index page and fix vh bug on other pages

--- pages/classes/[slug].jsx
+++ pages/classes/[slug].jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import Layout from '@/components/Layout/Layout'
 
--- pages/monsters/index.jsx
+++ pages/monsters/index.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaSearch } from 'react-icons/fa'
 
 import Layout from '@/components/Layout/Layout'
 
-import styles from '@/styles/pages/classes/Classes.module.scss'
-import { getClassList } from 'services/api-calls'
+import styles from '@/styles/pages/monsters/Monsters.module.scss'
+import { getMonsterList } from 'services/api-calls'
 
-export default function ClassesPage({ classes }) {
+export default function MonstersPage({ monsters }) {
   return (
     <Layout>
       <section className={styles.banner}>
         <h1>Monsters</h1>
         <h2>What are they?</h2>
+        <div className={styles.search}>
+          <input type="text" placeholder="Search..." />
+          <button>
+            <FaSearch />
+          </button>
+        </div>
       </section>
       <section className={styles.container}>
         <div className={styles.content}>
-          
+          {monsters.results.map((monster) => {
+            return (
+              <>
+                <Link href={`/monsters/${monster.index}`}>
+                  <p>{monster.name}</p>
+                </Link>
+              </>
+            )
+          })}
         </div>
       </section>
     </Layout>
@@ -24,10 +39,10 @@ export default function ClassesPage({ classes }) {
 }
 
 export async function getStaticProps() {
-  const res = await getClassList()
+  const res = await getMonsterList()
   return {
     props: {
-      classes: res,
+      monsters: res,
     },
   }
 }
